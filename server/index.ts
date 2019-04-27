b256b0d195c702988f616ebd9c4781452a1a1c53
Noah Loomans <noahloomans@gmail.com>
2019-04-27 15:20:39 +0200

    feat: add typescript to the server

---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+import * as next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+    .then(() => {
+        const server = express();
+
+        server.get('*', (req, res) => {
+            return handle(req, res);
+        });
+
+        server.listen(port, (err: Error) => {
+            if (err) throw err;
+            console.log(` > Ready on http://localhost:${port}`);
+        });
+    })
+    .catch(ex => {
+        console.error(ex.stack);
+        process.exit(1);
+    });
