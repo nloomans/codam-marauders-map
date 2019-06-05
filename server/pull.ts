25a69dd98d43ad712e8da5902aad01b25130aa5a
Noah Loomans <noahloomans@protonmail.com>
2019-06-05 15:28:02 +0200

    fix: auto refresh token

--- server/pull.ts
+++ server/pull.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 import httpLinkHeader from "http-link-header";
-import { create as makeOauth2 } from "simple-oauth2";
+import { create as makeOauth2, AccessToken, OAuthClient } from "simple-oauth2";
 import { ILocations } from "../types";
 
 if (process.env.UID === undefined || process.env.SECRET === undefined) {
@@ -23,9 +23,39 @@ const sleep = (ms: number) =>
 const pad = (str: string, n: number): string =>
     str.length < n ? pad(str + " ", n) : str;
 
-const makeGetAllPages = (axiosInstance: AxiosInstance) =>
-    async (url: string, acc = []): Promise<any> => {
-        const { data, headers } = await axiosInstance.get(url);
+interface ISendGetRequest {
+    (url: string): Promise<any>;
+}
+interface IGetAccessToken {
+    (): Promise<string>;
+}
+
+const makeGetAccessToken =
+    (oauth2Client: OAuthClient<"client_id">): IGetAccessToken => {
+        let tokenCache: AccessToken | null = null;
+
+        return async (): Promise<string> => {
+            if (tokenCache == null || tokenCache.expired()) {
+                tokenCache = oauth2Client.accessToken.create(
+                    await oauth2Client.clientCredentials.getToken({}));
+            }
+            return tokenCache.token.access_token;
+        }
+    }
+
+const makeSendGetRequest =
+    (axiosInstance: AxiosInstance, getAccessToken: IGetAccessToken): ISendGetRequest =>
+        async (url: string): Promise<any> => {
+            return axiosInstance.get(url, {
+                headers: {
+                    Authorization: `Bearer ${await getAccessToken()}`,
+                }
+            });
+        };
+
+const makeGetAllPages = (sendGetRequest: ISendGetRequest) =>
+    async function getAllPages(url: string, acc = []): Promise<any> {
+        const { data, headers } = await sendGetRequest(url);
         const totalData = acc.concat(data);
         const next = headers.link &&
             httpLinkHeader.parse(headers.link).get("rel", "next")[0];
@@ -33,23 +63,20 @@ const makeGetAllPages = (axiosInstance: AxiosInstance) =>
             return (totalData);
         }
         await sleep(500);
-        return (makeGetAllPages(axiosInstance)(next.uri, totalData));
+        return (getAllPages(next.uri, totalData));
     };
 
 export default async function pull(callback: (locations: ILocations) => void) {
-    const token = oauth2.accessToken.create(
-        await oauth2.clientCredentials.getToken({}),
-    );
-    const instance = axios.create({
-        headers: {
-            Authorization: `Bearer ${token.token.access_token}`,
-        },
-    });
+    const axiosInstance = axios.create();
+    const getAccessToken = makeGetAccessToken(oauth2);
+    const sendGetRequest = makeSendGetRequest(axiosInstance, getAccessToken);
+
     // TODO: replace any with proper types
-    const getAllPages = makeGetAllPages(instance);
+    const getAllPages = makeGetAllPages(sendGetRequest);
     while (true) {
         const rawLocations: any[] = await getAllPages(
-            "https://api.intra.42.fr/v2/campus/14/locations?per_page=100&filter[active]=true&filter[primary]=true",
+            "https://api.intra.42.fr/v2/campus/14/locations" +
+            "?per_page=100&filter[active]=true&filter[primary]=true",
         );
         const locations: ILocations = rawLocations.reduce((prev, curr) => {
             const { host, user: { login } } = curr;
